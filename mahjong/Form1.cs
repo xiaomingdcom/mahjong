27d09db3046d4f56a3d12c5cf568221eb76e9a99
xiaomingdcom <1184261526@qq.com>
2016-10-31 19:27:10 +0800

    10.31-1

--- mahjong/Form1.cs
+++ mahjong/Form1.cs
@@ -12,6 +12,59 @@ namespace mahjong
 {
     public partial class Form1 : Form
     {
+        string humanPlayer_card1 = "back";
+        string humanPlayer_card2 = "back";
+        string humanPlayer_card3 = "back";
+        string humanPlayer_card4 = "back";
+        string humanPlayer_card5 = "back";
+        string humanPlayer_card6 = "back";
+        string humanPlayer_card7 = "back";
+        string humanPlayer_card8 = "back";
+        string humanPlayer_card9 = "back";
+        string humanPlayer_card10 = "back";
+        string humanPlayer_card11 = "back";
+        string humanPlayer_card12 = "back";
+        string humanPlayer_card13 = "back";
+        string humanPlayer_card14 = "back";
+        string leftAIPlayer_card1 = "back";
+        string leftAIPlayer_card2 = "back";
+        string leftAIPlayer_card3 = "back";
+        string leftAIPlayer_card4 = "back";
+        string leftAIPlayer_card5 = "back";
+        string leftAIPlayer_card6 = "back";
+        string leftAIPlayer_card7 = "back";
+        string leftAIPlayer_card8 = "back";
+        string leftAIPlayer_card9 = "back";
+        string leftAIPlayer_card10 = "back";
+        string leftAIPlayer_card11 = "back";
+        string leftAIPlayer_card12 = "back";
+        string leftAIPlayer_card13 = "back";
+        string rightAIPlayer_card1 = "back";
+        string rightAIPlayer_card2 = "back";
+        string rightAIPlayer_card3 = "back";
+        string rightAIPlayer_card4 = "back";
+        string rightAIPlayer_card5 = "back";
+        string rightAIPlayer_card6 = "back";
+        string rightAIPlayer_card7 = "back";
+        string rightAIPlayer_card8 = "back";
+        string rightAIPlayer_card9 = "back";
+        string rightAIPlayer_card10 = "back";
+        string rightAIPlayer_card11 = "back";
+        string rightAIPlayer_card12 = "back";
+        string rightAIPlayer_card13 = "back";
+        string oppositeAIPlayer_card1 = "back";
+        string oppositeAIPlayer_card2 = "back";
+        string oppositeAIPlayer_card3 = "back";
+        string oppositeAIPlayer_card4 = "back";
+        string oppositeAIPlayer_card5 = "back";
+        string oppositeAIPlayer_card6 = "back";
+        string oppositeAIPlayer_card7 = "back";
+        string oppositeAIPlayer_card8 = "back";
+        string oppositeAIPlayer_card9 = "back";
+        string oppositeAIPlayer_card10 = "back";
+        string oppositeAIPlayer_card11 = "back";
+        string oppositeAIPlayer_card12 = "back";
+        string oppositeAIPlayer_card13 = "back";
         public Form1()
         {
             InitializeComponent();
